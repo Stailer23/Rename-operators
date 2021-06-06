f11561a99599f6d3520eb43eec9705617781859b
Stailer23 <stailer23@mail.ru>
2021-06-06 19:02:31 +0300

    Добавлены значение по умолчанию

--- rename_operators.py
+++ rename_operators.py
@@ -16,16 +16,19 @@ def go():
 l1=LabelFrame(root, text='Введите номер RTU')
 l1.place(x=10, y=5)
 e1= Entry(l1, width=30)
+e1.insert(0, '000072801210907')
 e1.pack()
 
 l2=LabelFrame(root, text = 'Что заменить')
 l2.place(x=10, y=50)
 e2=Entry(l2, width=20)
+e2.insert(0, 'RUSSIA VOTEK')
 e2.pack()
 
 l3=LabelFrame(root, text = 'На что заменить')
 l3.place(x=10, y=95)
 e3=Entry(l3, width=20)
+e3.insert(0, 'Tele2')
 e3.pack()
 
 l5=Label(root, text='by Nikolaev', fg='gray')
